0f97b47dadca0c8a793df81c18df3cff09cfcfd5
Michael Kjeldsen <1846219+mkjeldsen@users.noreply.github.com>
2025-02-06 11:13:50 +0100

    Update tagster_lib.php to v3.1

--- phpnews/tagster_lib.php
+++ phpnews/tagster_lib.php
@@ -1,78 +1,52 @@
 <?php
 /*
-	File : tagster_lib.php
-	Version : 2.2
-	Date : 18. april 2002
-	Author : Lars B. Jensen, lars.jensen@eug.dk
-
-	Module Description
-	Module to transform URL and E-Mail addresses into clickable links.
-
-	Note
-	This module library has support for the special scandinavian specialchars æ, ø and å. 
-
-	Public Functions
-	--------------------------------------------------------------´
-	function tagster_format($str)
-	function tagster_url($str, $target, $class)
-	function tagster_email($str, $class)
-
-	Private Functions
-	--------------------------------------------------------------´
-	function tagster_fix_endchar($str)
-	function tagster_expand($str)
-	function tagster_reduce($str)
+ Version : 3.0
+ Author : Lars B. Jensen, lars.jensen@ljweb.com
+ Version : 3.1
+ Author : Jacob Friis Larsen, jfl@eksperten.dk
+ Module Description
+ Module to transform URL and E-Mail addresses into clickable links.
+ Instruction
+ Use the function format to extract and modify all links and emails in a given string.
+ The parameters $target handling the target for links and $css_class for handling css on <a> tags are
+ both optional.
 */
-	function tagster_format($str, $target="_blank", $css_class="") {
-		$str = str_replace("&", "&amp;", $str);
-		$str = str_replace("<", "&lt;", $str);
-		$str = str_replace(">", "&gt;", $str);
-		$str = tagster_url($str, $target, $css_class);
-		$str = tagster_email($str, $css_class);
-		$str = tagster_fix_endchar($str);
-		$str = str_replace("  ", "&nbsp; ", $str);
-		$str = str_replace("\t", "&nbsp;&nbsp;&nbsp; ", $str);
-		$str = str_replace("\r", "", $str);
-		$str = str_replace("\n", "<br>", $str);
-		return $str;
-	}
-
-
-	function tagster_url($str, $target, $css_class) {
-		$ins_str = "";
-		if ($css_class) $ins_str .= " class=\"".$css_class."\"";
-		if ($target) $ins_str .= " target=\"".$target."\"";
-		$str = tagster_expand($str);
-		$str = preg_replace ("/(ftp|http|https|telnet|news|nntp|file|irc):\/\/([a-z0-9~#%@&:;=!',_æøå\(\)\?\/\.\-\+\[\]\|\*\$\^\{\}]+)/i", "<a href=\"\\1://\\2\"".$ins_str.">\\1://\\2</a>", $str);
-		$str = preg_replace ("/(\s|tp\:|\(\[)(www\.)([a-z0-9~#%@&:;=!',_æøå\(\)\?\/\.\-\+\[\]\|\*\$\^\{\}]+)/i", "\\1<a href=\"http://\\2\\3\"".$ins_str.">\\2\\3</a>", $str);
-		$str = preg_replace ("/(\s|tp\:|\(\[)(ftp\.)([a-z0-9~#%@&:;=!',_æøå\(\)\?\/\.\-\+\[\]\|\*\$\^\{\}]+)/i", "\\1<a href=\"ftp://\\2\\3\"".$ins_str.">\\2\\3</a>", $str);
-		return tagster_reduce($str);
-	}
-
-
-	function tagster_email($str, $css_class="") {
-		$ins_class = "";
-		if ($css_class) $ins_class = " class=\"".$css_class."\"";
-		$str = tagster_expand($str);
-		$str = preg_replace ("/([\s|\"])([\w|\.|\-|_]+)@([\w||\-|_]+)\.([\w|\.|\-|_]+)/i", "\\1<a href=\"mailto:\\2@\\3.\\4\"".$ins_class.">\\2@\\3.\\4</a>", $str);
-		return tagster_reduce($str);
-	}
-
-
-	function tagster_fix_endchar($str) {
-		$str = preg_replace ("/([\'\"\)\]\.\,\?\!]+)\">/i", "\">", $str);
-		$str = preg_replace ("/([\'\"\)\]\.\,\?\!]+)\" (target|class)=\"/i", "\" \\2=\"", $str);
-		$str = preg_replace ("/([\'\"\)\]\.\,\?\!]+)<\/a>/i", "</a>\\1", $str);
-		return $str;
-	}
-
-
-	function tagster_expand($str) {
-		return " ".$str." ";
-	}
-
-
-	function tagster_reduce($str) {
-		return substr($str, 1, -1);
-	}
+ class tagster {
+ function format ($str, $target = '_blank', $css_class = '') {
+ $str = str_replace(array('&', '<', '>'), array('&amp;', '&lt;', '&gt;'), $str);
+ $str = $this->url($str, $target, $css_class);
+ //$str = $this->email($str, $css_class);
+ $str = $this->fix_endchar($str);
+ $str = str_replace(array(' ', "\t", "\r", "\n"), array('&nbsp; ', '&nbsp; &nbsp; ', '', '<br />'), $str);
+ return $str;
+ }
+ function url ($str, $target, $css_class) {
+ $ins_str = ($css_class != '') ? ' class="'.$css_class.'"' : '';
+ if ($target != '') {
+ $ins_str .= ' target="'.$target.'"';
+ }
+ $str = $this->expand($str);
+ $str = preg_replace(array('/(ftp|http|https|telnet|news|nntp|file|irc):\/\/([az0-9~#%@&:;=!\',_Ã¦Ã¸Ã¥\(\)\?\/\.\-\+\[\]\|\*\$\^\{\}]+)/i', '/(\s|tp\:|\(|\[|\&gt;)(www\.)([az0-9~#%@&:;=!\',_Ã¦Ã¸Ã¥\(\)\?\/\.\-\+\[\]\|\*\$\^\{\}]+)/i', '/(\s|tp\:|\(|\[|\&gt;)(ftp\.)([az0-9~#%@&:;=!\',_Ã¦Ã¸Ã¥\(\)\?\/\.\-\+\[\]\|\*\$\^\{\}]+)/i'), array('<a
+href="\\1://\\2"'.$ins_str.'>\\1://\\2</a>', '\\1<a href="http://\\2\\3"'.$ins_str.'>\\2\\3</a>', '\\1<a
+href="ftp://\\2\\3"'.$ins_str.'>\\2\\3</a>'), $str);
+ return $this->reduce($str);
+ }
+ function email ($str, $css_class = '') {
+ $ins_class = ($css_class != '') ? ' class="'.$css_class.'"' : '';
+ $str = $this->expand($str);
+ $str = preg_replace('/([\s"])([\w\.\-_]+)@([\w\-_]+)\.([\w\.\-_]+)/i', '\\1<a
+href="mailto:\\2@\\3.\\4"'.$ins_class.'>\\2@\\3.\\4</a>', $str);
+ return $this->reduce($str);
+ }
+ function fix_endchar ($str) {
+ return preg_replace(array('/([\'"\)\]\.\,\?\!]+)">/i', '/([\'\"\)\]\.\,\?\!]+)" (target|class)="/i',
+'/([\'\"\)\]\.\,\?\!]+)<\/a>/i'), array('">', '" \\2="', '</a>\\1'), $str);
+ }
+ function expand ($str) {
+ return ' '.$str.' ';
+ }
+ function reduce ($str) {
+ return substr($str, 1, -1);
+ }
+ }
 ?>
